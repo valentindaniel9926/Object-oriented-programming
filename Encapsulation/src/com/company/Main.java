b7209b003ff5fb59c23a96d18df36715891dc469
Valentin Mitroi <64599071+valentindaniel9926@users.noreply.github.com>
2021-04-28 18:00:33 +0300

    Add files via upload

---
+++ Encapsulation/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	;/*Player player = new Player();
+		player.name= "Tim";
+		player.health=20;
+		player.weapon="Sword";
+
+		int damage=10;
+		player.loseHealth(damage);
+		System.out.println("Remaining Health= " + player.healthRemaining());
+
+		damage=11;
+		player.loseHealth(damage);
+		System.out.println("Remaining Health= " + player.healthRemaining())*/
+
+		EnhancedPlayer player = new EnhancedPlayer("Valentin",200,"Sword");
+		System.out.println("Initial health is " + player.getHitPoints());
+    }
+}
